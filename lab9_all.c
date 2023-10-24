72d23f4e9676d507756fbd00226da4e44f29811f
dying 2 live <76590685+pwaradowx@users.noreply.github.com>
2023-10-24 21:35:13 +0300

    Create lab9_all.c

---
+++ lab9_all.c
@@ -0,0 +1,117 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <locale.h>
+#define ROW 9
+#define COL 9
+
+void basic_pifagor()
+{
+	for (int i = 1; i <= ROW; i++)
+	{
+		for (int j = 1; j <= COL; j++)
+		{
+			printf("%2d ", i * j);
+		}
+
+		printf("\n");
+	}
+}
+
+void left_low_pifagor()
+{
+	int row = 9;
+	int col = 0;
+
+	for (int i = 2; i <= row; i++)
+	{
+		col++;
+
+		for (int j = 1; j <= col; j++)
+		{
+			printf("%2d ", i * j);
+		}
+
+		printf("\n");
+	}
+}
+
+void task1()
+{
+	int row = 9;
+	int col = 0;
+
+	int start = 31;
+	int a = 31;
+
+	for (int i = 2; i <= row; i++)
+	{
+		a = start - i + 1;
+		col++;
+
+		for (int j = 1; j <= col; j++)
+		{
+			printf("%2d ", a);
+			a++;
+		}
+
+		printf("\n");
+	}
+}
+
+void task2()
+{
+	int input;
+
+	printf("Введите число: ");
+	scanf("%d", &input);
+
+	int a = 0;
+	int b = 0;
+	int counter = 1;
+
+	while (input != 0)
+	{
+		if (counter % 2 != 0)
+		{
+			a += input % 10;
+		}
+		else
+		{
+			b += input % 10;
+		}
+
+		input /= 10;
+		counter++;
+	}
+
+	printf("Результат равен: %d", a * b);
+}
+
+void task3()
+{
+	char symb;
+	int high;
+	int length;
+
+	printf("Введите символ из которого будет строиться фигура: ");
+	scanf("%c", &symb);
+	printf("\nВведите высоту: ");
+	scanf("%d", &high);
+	printf("\nВведит длину верхней грани: ");
+	scanf("%d", &length);
+
+	for (int i = 1; i <= length; i++)
+	{
+		for (int j = 1; j <= high; j++)
+		{
+			printf("%c", symb);
+		}
+	}
+}
+
+void main()
+{
+	setlocale(LC_ALL, "Russian");
+
+	task3();
+}
