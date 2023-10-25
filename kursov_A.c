416c428de6080f4768fe99e31ef58bf2ba67875b
dying 2 live <76590685+pwaradowx@users.noreply.github.com>
2023-10-25 10:27:15 +0300

    Update kursov_A.c

--- kursov_A.c
+++ kursov_A.c
@@ -6,54 +6,13 @@
 #include <math.h>
 #include <string.h>
 
-// Функуция для запроса у пользователя на продолжение и проверки корректности принимаемого значения.
-char continue_request()
-{
-	char ans = '0';
-	bool correct_val = ans == 'y' || ans == 'n';
-
-	while (!correct_val)
-	{
-		printf("Желаете продолжить? (y/n): ");
-
-		scanf(" %c", &ans);
-
-		correct_val = ans == 'y' || ans == 'n';
-
-		if (!correct_val) 
-		{ 
-			printf("Введено некорректное значение!\n");
-		}
-		else if (ans == 'n')
-		{
-			printf("Закрываем программу...");
-			exit(EXIT_SUCCESS);
-		}
-	}
-
-	return ans;
-}
-
-// Функция интерфейса для ознакомления пользователя с функционалом программы.
-void welcome_menu()
-{
-	printf("Добро пожаловать!\n");
-	printf("Эта программа предназначена для расчета количества штукатурки для комнаты.\n");
-	
-	continue_request();
-}
-
-void setup_start_data(float *rash, float *thick)
+void get_rashod_and_thick(float* rash, float* thick)
 {
 	printf("Введите расход шпаклевки на 1 м2, указанный на упаковке: ");
 	scanf("%f", &*rash);
 
-	continue_request();
-
 	printf("Введите желаемую толщину слоя: ");
 	scanf("%f", &*thick);
-
-	continue_request();
 }
 
 float get_room_square()
@@ -62,8 +21,6 @@ float get_room_square()
 
 	printf("Введите суммарную площадь стен, потолка и пола: ");
 	scanf("%f", &room_square);
-
-	continue_request();
 
 	return room_square;
 }
@@ -82,49 +39,76 @@ void calculate_shtukaturka(float holes_sq, float room_sq, float thickness, float
 {
 	float result;
 
-	float rashod_sht;
-	float layer_thickness;
-
 	float square_to_work = room_sq - holes_sq;
 
 	printf("Площадь для покрытия штукатуркой равна %.3f\n", square_to_work);
 
-	setup_start_data(&rashod_sht, &layer_thickness);
-
 	printf("Считаем нужное количество штукатурки...\n");
 
-	if (thickness == 0)
-	{
-		result = square_to_work * rashod_sht * layer_thickness + 1;
-	}
-	else
-	{
-		result = square_to_work * rashod * thickness + 1;
-	}
+	result = square_to_work * rashod * thickness + 1;
 
-	printf("Количество штукатурки, нужное для покрытия вашей комнаты составляет %d кг", (int)result);
+	printf("Количество штукатурки, нужное для покрытия вашей комнаты составляет %d кг\n", (int)result);
+}
+
+// Функция интерфейса меню.
+void menu()
+{
+	printf("\nМеню: \n");
+	printf("1) Ввести общую площадь комнаты\n");
+	printf("2) Ввести площадь окон и дверей\n");
+	printf("3) Ввести желаемую толщину слоя покрытия\n   И расход штукатурки на 1кв. метр\n");
+	printf("4) Посчитать нужное количество штукатурки\n");
+	printf("5) Выход\n");
 }
 
 void manual_input()
 {
-	welcome_menu();
-	calculate_shtukaturka(get_holes_square(), get_room_square(), 0, 0);
-}
+	float room_square = 0;
+	float holes_square = 0;
+	float thickness = 0;
+	float rashod = 0;
 
-void file_io()
-{
-	FILE* file;
+	int ans = 0;
 
-	file = fopen("inputdata.txt", "r");
-	float data[4];
-	int i = 0;
+	printf("Добро пожаловать!\n");
+	printf("Эта программа предназначена для расчета количества штукатурки для комнаты.\n");
 
-	while (fscanf(file, "%f", &data[i]) != EOF)
+	do
 	{
-		i++;
-	}
+		menu();
 
-	calculate_shtukaturka(data[1], data[0], data[3], data[2]);
+		printf("Введите номер команды: ");
+		scanf("%d", &ans);
+
+		switch (ans)
+		{
+		case 1:
+			room_square = get_room_square();
+			break;
+		case 2:
+			holes_square = get_holes_square();
+			break;
+		case 3:
+			get_rashod_and_thick(&rashod, &thickness);
+			break;
+		case 4:
+			if (room_square != 0 && holes_square != 0 && thickness != 0 && rashod != 0)
+			{
+				calculate_shtukaturka(holes_square, room_square, thickness, rashod);
+				break;
+			}
+			else
+			{
+				printf("Введены не все необходимые значения!\n");
+				continue;
+			}
+		case 5:
+			exit(EXIT_SUCCESS);
+		default:
+
+			exit(EXIT_SUCCESS);
+		}
+	} while (ans <= 5);
 }
 
 void main()
