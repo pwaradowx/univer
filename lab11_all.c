4461a7afd43d4cf93da2dc43f6b6d9d1c1751322
dying 2 live <76590685+pwaradowx@users.noreply.github.com>
2023-11-07 21:14:58 +0300

    Update lab11_all.c

--- lab11_all.c
+++ lab11_all.c
@@ -6,7 +6,7 @@
 
 void task1()
 {
-	float A[10];
+    float A[10];
     float B[10];
 
     for (int i = 0; i < N; i++)
@@ -49,42 +49,87 @@ void task1()
 task2()
 {
     float A[100];
-    float x = 1;
-    float y;
+    float B[21];
+    float plussum = 0;
+    int pluscount = 0;
+    float minsum = 0;
+    int mincount = 0;
 
-    for (int i = 0; i < 100; i++)
+    float x = 2;
+
+    for (int i = 0; i < 21; i++)
     {
-        A[i] = abs(log(x)) - pow(x - 2, 2);
-        x += 1;
+        B[i] = fabs(log(x)) - pow(x-2, 2);
+        x += 0.1;
     }
 
-    float plussum = 0;
-    int pluscounter = 0;
-    float minsum = 0;
-    int mincounter = 0;
+    int j = 0;
 
     for (int i = 0; i < 100; i++)
     {
+        if (j == 21) j = 0;
+        A[i] = B[j];
+
         if (A[i] > 0)
         {
             plussum += A[i];
-            pluscounter++;
+            pluscount++;
         }
-        else if (A[i] < 0)
+        else
         {
             minsum += A[i];
-            mincounter++;
+            mincount++;
+        }
+        j++;
+    }
+
+    printf("Имя массива - А; Сумма положительных - %lf; Сумма отрицательных - %lf; Число положительных - %d; Число отрицательных - %d; Среднее значние - %lf",
+        plussum, minsum, pluscount, mincount, (plussum + minsum) / 100);
+}
+
+void task3()
+{
+    int n;
+    int A[100];
+
+    float srednee = 0;
+
+    printf("Введите количество элементов массива: ");
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++)
+    {
+        int temp = 0;
+
+        printf("A[%d] = ", i);
+        scanf("%d", &temp);
+
+        A[i] = temp;
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        srednee += A[i];
+    }
+
+    srednee /= n;
+
+    int count = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        if (A[i] < srednee)
+        {
+            count++;
         }
     }
 
-    float plussrednee = plussum / pluscounter;
-
-    printf("A[]: %.3f  %.3f  %d  %d  %.3f", plussum, minsum, pluscounter, mincounter, plussrednee);
+    printf("Количество элементов, которые меньше среднего арифм. - %d", count);
 }
 
 void main()
 {
     setlocale(LC_ALL, "Russian");
 
-    task2();
+    task3();
 }
